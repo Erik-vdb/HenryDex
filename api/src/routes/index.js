443778dcb6972c99328061aed39f4be49867661f
Exivac <exivacz@gmail.com>
2022-08-19 13:33:44 -0300

    Looking for ID

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -168,7 +168,7 @@ router.post('/pokemons', async (req, res) => {
 
 router.delete('/pokemons', async (req, res) => {
   const {ID} = req.body
-  console.log(req.body)
+  console.log(req)
   try {
     await Pokemon.destroy({where: {ID:ID}})
     res.status(202).send()
