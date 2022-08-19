8dadaba7745e83866c8eab75c7394a96eb6ccd74
Exivac <exivacz@gmail.com>
2022-08-19 13:27:15 -0300

    deploy

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -168,6 +168,7 @@ router.post('/pokemons', async (req, res) => {
 
 router.delete('/pokemons', async (req, res) => {
   const {ID} = req.body
+  console.log(ID)
   try {
     await Pokemon.destroy({where: {ID:ID}})
     res.status(202).send()
