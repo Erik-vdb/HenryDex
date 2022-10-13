a3dc6a84e6fcf04b7b448fe1548d3a3d01c8ae6f
Erik-vdb <vandenerik824@gmail.com>
2022-10-13 17:14:42 -0300

    Fixing pokemon deletion

--- api/src/routes/deletePokemon.js
+++ api/src/routes/deletePokemon.js
@@ -2,6 +2,7 @@ const { Pokemon } = require('../db')
 
 module.exports = async function (req, res) {
   const {ID} = req.body
+  console.log(ID)
   try {
     await Pokemon.destroy({where: {ID}})
     res.status(202).send("Eliminated Succesfully")
